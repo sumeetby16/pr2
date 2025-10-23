4f90e6e46900644fcb381edb74ad01d619e05636
sumeetby16 <sumeetby16>
2025-10-23 14:35:24 +0530

    CI

--- pr2.py
+++ pr2.py
@@ -5,3 +5,13 @@ T = 3
 SI = (P * R * T) / 100
 
 print(f"Simple Interest: ₹{SI}")
+
+P = 10000
+R = 5
+T = 3
+
+SI = (P * R * T) / 100
+CI = P * ((1 + R / 100) ** T) - P
+
+print(f"Simple Interest: ₹{SI}")
+print(f"Compound Interest: ₹{round(CI, 2)}")
